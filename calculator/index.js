471426f22393b26abe67c6b6fa08a213e9741c7d
joshdels <deleonjoshy2@gmail.com>
2025-02-23 18:28:23 +0800

    finished skeleton calculator

--- calculator/index.js
+++ calculator/index.js
@@ -1,8 +1,10 @@
+var answer = document.getElementById('answer')
+
 function calculate() {
     var first_number = document.getElementById('number1').value
     var second_number = document.getElementById('number2').value
     var operation = document.getElementById('operation').value
-    var answer = document.getElementById('answer')
+
 
     switch (operation) {
     case "+":
@@ -18,4 +20,16 @@ function calculate() {
         answer.innerHTML = parseInt(first_number) / parseInt(second_number)
         break;    
     }
+}
+
+function solve1() {
+    var sides = document.getElementById('sides').value
+
+    answer.innerHTML = parseInt(sides)** 2 + ' m2'
+}
+
+function solve2() {
+    var sides = document.getElementById('sides').value
+
+    answer.innerHTML = parseInt(sides)** 3 + ' m2'
 }
