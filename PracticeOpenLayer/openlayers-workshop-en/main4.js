8acea108be2ae957558c11abc7af94592199da16
joshdels <deleonjoshy2@gmail.com>
2025-03-02 15:17:03 +0800

    workshop openlayers

--- PracticeOpenLayer/openlayers-workshop-en/main4.js
+++ PracticeOpenLayer/openlayers-workshop-en/main4.js
@@ -7,6 +7,7 @@ import {StadiaMaps, Vector as VectorSource} from 'ol/source';
 import {fromLonLat} from 'ol/proj';
 import {parse} from 'papaparse'; //mao ni na module mo change sa csv to readable js
 import WebGLVectorLayer from 'ol/layer/WebGLVector'; // to read dynamic styling 
+import { V } from 'ol/renderer/webgl/FlowLayer';
 
 // mao ni ang source na csv na gi parse
 const source = new VectorSource();
@@ -29,17 +30,43 @@ parse('./data/meteorites.csv', {
   },
 });
 
+const minYear = 1850;
+const maxYear = 2015;
+const span = maxYear - minYear;
+const rate = 10; // years per second
+
+const start = Date.now();
+
+// Time filteres
+const period = 10;
+const periodStart = ['-', ['var', 'currentYear'], period];
+const decay = [
+  'interpolate',
+  ['linear'],
+  ['get', 'year'],
+  periodStart,
+  0,
+  ['var', 'currentYear'],
+  1,
+];
+
 // styling sa parsed na data
 const meteorites = new WebGLVectorLayer({
+  filter: ['between', ['get', 'year'], periodStart, ['var', 'currentYear']],
   source: source,
   style: {
     'circle-radius': [
-        '+',
-        ['*', ['clamp', ['*', ['get', 'mass'], 1 / 20000], 0, 1], 9],
-        4,
-      ],
-    'circle-fill-color': 'rgba(255, 0, 0, 0.5)',
+      '*',
+      decay,
+      ['+', ['*', ['clamp', ['*', ['get', 'mass'], 1 / 20000], 0, 1], 9], 4],
+    ],
+    'circle-fill-color': ['color', 255, 0, 0, ['*', 0.5, decay]],
   },
+  variables: {
+    currentYear: minYear,
+  },
+  
+  disableHitDetection: true,
 });
 
 
@@ -59,3 +86,41 @@ new Map({
     zoom: 2,
   }),
 });
+
+
+
+// This is the time frame area
+const yearElement = document.getElementById('year');
+
+// Create a legend element
+const legendElement = document.getElementById('legend');
+
+function updateLegend() {
+  legendElement.innerHTML = `
+    <h1>Legend</h1>
+    <div style="display: flex; align-items: center;">
+      <div style="width: 20px; height: 20px; background-color: rgba(255, 0, 0, 0.5); border-radius: 50%; margin-right: 10px;"></div>
+      <span>Meteorite</span>
+    </div>
+  `;
+}
+
+function render() {
+  const elapsed = (rate * (Date.now() - start)) / 1000;
+  const currentYear = Math.round(minYear + (elapsed % span));
+  meteorites.updateStyleVariables({currentYear: currentYear});
+  yearElement.innerText = currentYear;
+  
+  updateLegend();
+  requestAnimationFrame(render);
+
+
+}
+
+
+
+render();
+
+
+
+
